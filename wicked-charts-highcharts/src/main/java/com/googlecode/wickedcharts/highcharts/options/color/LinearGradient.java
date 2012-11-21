de1727b9a4213e4ef5ab16b5dbf5e664d7cc68f5
Tom Hombergs <tom.hombergs@gmail.com>
2012-11-21 21:08:04 +0000

    updated license notice

--- wicked-charts-highcharts/src/main/java/com/googlecode/wickedcharts/highcharts/options/color/LinearGradient.java
+++ wicked-charts-highcharts/src/main/java/com/googlecode/wickedcharts/highcharts/options/color/LinearGradient.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Tom Hombergs (tom.hombergs@gmail.com)
+ *   Copyright 2012 Wicked Charts (http://wicked-charts.googlecode.com)
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
