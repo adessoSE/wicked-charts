f4c35365fe54639aad9e2101c4b45c82e8ea5ce3
Tom Hombergs <tom.hombergs@gmail.com>
2013-06-10 20:50:04 +0000

    preparing release 1.5.0

--- wicked-charts-parent/rename_signed_files.bat
+++ wicked-charts-parent/rename_signed_files.bat
@@ -1,5 +1,10 @@
 MOVE /Y ..\wicked-charts-parent\pom.xml.sig ..\wicked-charts-parent\pom.asc
 
+MOVE /Y ..\wicked-charts-wicket14\pom.xml.sig ..\wicked-charts-wicket14\pom.asc
+MOVE /Y ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2-javadoc.jar.sig ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2-javadoc.jar.asc
+MOVE /Y ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2-sources.jar.sig ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2-sources.jar.asc
+MOVE /Y ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2.jar.sig ..\wicked-charts-wicket14\target\wicked-charts-wicket14-1.4.2.jar.asc
+
 MOVE /Y ..\wicked-charts-wicket15\pom.xml.sig ..\wicked-charts-wicket15\pom.asc
 MOVE /Y ..\wicked-charts-wicket15\target\wicked-charts-wicket15-1.4.2-javadoc.jar.sig ..\wicked-charts-wicket15\target\wicked-charts-wicket15-1.4.2-javadoc.jar.asc
 MOVE /Y ..\wicked-charts-wicket15\target\wicked-charts-wicket15-1.4.2-sources.jar.sig ..\wicked-charts-wicket15\target\wicked-charts-wicket15-1.4.2-sources.jar.asc
