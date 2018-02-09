65715e3fe632b34c711478cf8d4f47f201740303
Atanasov <Maksim.Atanasov@adesso.de>
2018-02-09 20:52:16 +0100

    delete

--- chartjs-wrapper/src/main/java/com/googlecode/wickedcharts/chartjs/chartoptions/Stacked.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.wickedcharts.chartjs.chartoptions;
-
-import lombok.experimental.Accessors;
-
-@Accessors(chain = true)
-@lombok.Data
-public class Stacked {
-	private Boolean stacked;
-}
--- highcharts-wrapper/src/main/java/com/googlecode/wickedcharts/highcharts/options/drilldown/drilldown.js
+++
@@ -1,7 +0,0 @@
-function drilldown(point, drilldownOptionsArray) {
-	var index = point.drilldownOptionsIndex;
-	var drilldownOptions = drilldownOptionsArray[index];
-	if (drilldownOptions) {
-		new Highcharts.Chart(drilldownOptions);
-	}
-}
