b91ddd8fbfa34bdf5914f45d32c883523d0849c2
atanasov <Maksim.Atanasov@adesso.de>
2018-02-09 23:51:58 +0100

    Added Stacked Line Chart

--- chartjs-wrapper/src/main/java/com/googlecode/wickedcharts/chartjs/chartoptions/AxesScale.java
+++ chartjs-wrapper/src/main/java/com/googlecode/wickedcharts/chartjs/chartoptions/AxesScale.java
@@ -5,9 +5,9 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @lombok.Data
 public class AxesScale {
+	private Boolean stacked;
 	private Boolean display;
 	private ScaleLabel scaleLabel;
-	private Boolean stacked;
 	private String id;
 	private String position;
 	private String type;
--- showcase/wicked-charts-showcase-wicket7/src/main/java/com/googlecode/wickedcharts/showcase/HomepageChartJs.java
+++ showcase/wicked-charts-showcase-wicket7/src/main/java/com/googlecode/wickedcharts/showcase/HomepageChartJs.java
@@ -84,6 +84,8 @@ public class HomepageChartJs extends WebPage {
                 new PieChartConfiguration())); 
         this.add(new UpdateChartJsLink("AreaLineBoundariesChart", chart, codeContainer,
                 new AreaLineBoundariesChartConfiguration())); 
+        this.add(new UpdateChartJsLink("LineChartStacked", chart, codeContainer,
+                new LineChartStackedConfiguration()));
         this.add(new UpdateChartJsLink("ComboBarLineChart", chart, codeContainer,
                 new ComboBarLineChartConfiguration())); 
         this.add(new UpdateChartJsLink("LegendPositionTop", chart, codeContainer,
