a19de7e1449859d10fb6bbae2252bdb1c0544cac
Sven Wirz <sven.wirz@adesso.de>
2018-02-12 10:01:29 +0100

    Refactored SingleElementListObjectMapper

--- chartjs-wrapper/src/main/java/com/googlecode/wickedcharts/chartjs/jackson/serializer/SingleElementListSerializer.java
+++ chartjs-wrapper/src/main/java/com/googlecode/wickedcharts/chartjs/jackson/serializer/SingleElementListSerializer.java
@@ -6,20 +6,14 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.googlecode.wickedcharts.chartjs.jackson.SingleElementListObjectMapper;
 
 public class SingleElementListSerializer extends JsonSerializer<List<?>> {
 
-	
 	@Override
 	public void serialize(List<?> value, JsonGenerator gen, SerializerProvider serializers)
 			throws IOException, JsonProcessingException {
-		if (!serializers.getConfig().isEnabled(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED)) {
-			SingleElementListObjectMapper.getInstance().enable(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED).writeValue(gen, value);
-		} else {
-			gen.writeObject(value);
-		}
+		SingleElementListObjectMapper.getInstance().writeValue(gen, value);
 	}
 }
