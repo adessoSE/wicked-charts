e29533f28d6ab02f8e34e12be365ba99307aa762
atanasov <Maksim.Atanasov@adesso.de>
2018-02-19 14:59:10 +0100

    Worked on issue #122
    Added the following serializer tests:
    
    ConstValueSerializerTest
    DoubleValueSerializerTest
    FloatValueSerializerTest
    IntegerValueSerializerTest
    StringValueSerializerTest

---
+++ chartjs-wrapper/src/test/java/de/adesso/wickedcharts/chartjs/jackson/serializer/IntegerValueSerializerTest.java
@@ -0,0 +1,49 @@
+package de.adesso.wickedcharts.chartjs.jackson.serializer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.adesso.wickedcharts.chartjs.chartoptions.valueType.IntegerValue;
+import de.adesso.wickedcharts.chartjs.jackson.JsonRenderer;
+
+public class IntegerValueSerializerTest {
+	
+	private JsonRenderer renderer;
+	private IntegerValue testVal;
+	
+	@Before
+	public void setUp() throws Exception{
+		renderer = new JsonRenderer();
+	}
+	
+	@Test
+	public void testIntegerValue() {
+		testVal = new IntegerValue(100);
+		String json = renderer.toJson(testVal);
+		assertEquals("100", json);
+	}
+	
+	@Test
+	public void testIntegerMaxValue() {
+		testVal = new IntegerValue(Integer.MAX_VALUE);
+		String json = renderer.toJson(testVal);
+		assertEquals(new Integer(Integer.MAX_VALUE).toString(), json);
+	}
+	
+	
+	@Test
+	public void testIntegerMinValue() {
+		testVal = new IntegerValue(Integer.MIN_VALUE);
+		String json = renderer.toJson(testVal);
+		assertEquals(new Integer(Integer.MIN_VALUE).toString(), json);
+	}
+	
+	@Test
+	public void testNullValue() {
+		testVal = new IntegerValue();
+		String json = renderer.toJson(testVal);
+		assertEquals("", json);
+	}
+}
