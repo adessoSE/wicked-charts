0058e0e8bfa04c6b4d9ba1e31ecf5a4390d928f3
Alexander Nedomansky <34765163+anedomansky@users.noreply.github.com>
2018-04-13 11:30:03 +0200

    fixed javadoc comments.

--- highcharts-wrapper/src/main/java/de/adesso/wickedcharts/highcharts/options/util/OptionsUtil.java
+++ highcharts-wrapper/src/main/java/de/adesso/wickedcharts/highcharts/options/util/OptionsUtil.java
@@ -56,6 +56,11 @@ public class OptionsUtil {
 
     /**
      * Null-safe setter for the chart.events.load configuration.
+     *
+     * @param options
+     *
+     * @param function 
+     *
      */
     public void setChartEventsLoad(final Options options, final Function function) {
         if (options.getChartOptions() == null) {
@@ -157,6 +162,12 @@ public class OptionsUtil {
      * Retrieves the {@link Series} object with the given wickedChartsId from the
      * given {@link Options} object. Returns null if a Series with the given ID
      * does not exist.
+     *
+     * @param options Chartoptions
+     *
+     * @param wickedChartsId corresponding ID
+     *
+     * @return Series object
      */
     public static Series<?> getSeriesWithWickedChartsId(final Options options, final int wickedChartsId) {
         for (Series<?> series : options.getSeries()) {
@@ -171,6 +182,12 @@ public class OptionsUtil {
      * Retrieves the {@link Point} object with the given wickedChartsId from the
      * given {@link Options} object. Returns null if a Point with the given ID
      * does not exist.
+     *
+     * @param options Chartoptions
+     *
+     * @param wickedChartsId corresponding ID
+     *
+     * @return Point object
      */
     public static Point getPointWithWickedChartsId(final Options options, final int wickedChartsId) {
         for (Series<?> series : options.getSeries()) {
